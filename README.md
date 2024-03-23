c0339f6b585d0f5ef7a194e7d7a65cb072292f7b
Andysword <andrewhuang2019@gmail.com>
2024-03-23 11:42:34 -0500

    Updated main and README
    updated main with the functions specified in the lab. README now contains a description of the lab in it

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Lab6
+# Lab #6
+## IDE, Compiled Programs (C++ Programming), Git
+This lab tasked us with reading data from a file, and transforming that data to matrices.  
+Functions include (in order):  
+readFile - reads data from matrix_input and stores it into 2d vectors.  
+printMatrix - prints the matrix in a easy to see format.  
+addMatrix - adds matrix 1 and matrix 2 together, returns and prints the resulting matrix.  
+multiplyMatrix - multiplies matrix 1 and matrix 2, returns and prints the resulting matrix.  
+subtractMatrix - subtracts matrix 2 from matrix 1, returns and prints the resulting matrix.  
+updateMatrix - updates matrix 1 with value in row and column and prints the matrix. 
+maxValue - prints the max value of matrix 1. 
+transposeMatrix - transposes matrix 1 and prints the matrix. 
+main - runs all of the functions.  
--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <fstream>
 #include <iostream>
-#include <string>
 #include <vector>
 using namespace std;
+
+static vector<vector<int> > matrix1;
+static vector<vector<int> > matrix2;
 
 vector<int> readFile(string fileName){
     fstream fileStream;
@@ -14,12 +16,7 @@ vector<int> readFile(string fileName){
         fileCharacters.push_back(num);
     }
     fileStream.close(); 
-    
-    //static int fileIntegers[100];
-    /*
-    for(int i = 0; i < sizeof(fileCharacters); i++){
-        cout << fileCharacters[i] << "\n";
-    }*/
+
     return fileCharacters;
 }
 
@@ -30,6 +27,7 @@ void printMatrix(vector<vector<int> > matrix){
         }
         cout << "\n";
     }
+    cout << "\n";
 }
 
 vector<vector<int> > addMatrix(vector<vector<int> > matrix1, vector<vector<int> > matrix2){
@@ -40,43 +38,98 @@ vector<vector<int> > addMatrix(vector<vector<int> > matrix1, vector<vector<int> 
             returnedMatrix[row][col] = matrix1[row][col] + matrix2[row][col];
         }
     }
+    printMatrix(returnedMatrix);
 
     return returnedMatrix;
 
 }
-/*
+
 vector<vector<int> > multiplyMatrix(vector<vector<int> > matrix1, vector<vector<int> > matrix2){
-    
+    vector<vector<int> > returnedMatrix (matrix1.size(), vector<int> (matrix1.size())); 
+    for(int row = 0; row < matrix1.size(); row++){
+        for(int col = 0; col < matrix1[row].size(); col++){
+            int sum = 0;
+            for(int size = 0; size < matrix1.size(); size++){
+                sum += matrix1[row][size] * matrix2[size][col];
+            }
+
+            returnedMatrix[row][col] = sum;
+        }
+    }
+
+    printMatrix(returnedMatrix);
+
+    return returnedMatrix;
 }
+
 
 vector<vector<int> > subtractMatrix(vector<vector<int> > matrix1, vector<vector<int> > matrix2){
+    vector<vector<int> > returnedMatrix (matrix1.size(), vector<int> (matrix1.size()));
+
+    for(int row = 0; row < matrix1.size(); row++){
+        for(int col = 0; col < matrix1[row].size(); col++){
+            returnedMatrix[row][col] = matrix1[row][col] - matrix2[row][col];
+        }
+    }
+
+    printMatrix(returnedMatrix);
+
+    return returnedMatrix;
+}
+
+
+void updateMatrix(int row, int col, int value){
+    if (row > matrix1.size() -1 || row < 0){
+        cout << "Invalid row size" << "\n";
+    } 
+    else if (col > matrix1[0].size() -1 || col < 0){
+        cout << "Invalid column size" << "\n";
+    } else {
+    matrix1[row][col] = value; 
+    printMatrix(matrix1);
+    }
+}
+
+
+void maxValue(vector<vector<int> > matrix){
+
+    int maximumValue = INT_MIN;
+    for(int row = 0; row < matrix.size(); row++){
+        for(int col = 0; col < matrix[row].size(); col++){
+            if(matrix[row][col] > maximumValue){
+                maximumValue = matrix[row][col];
+            }
+        }
+    }
+    cout << "Max Value: " << maximumValue << "\n\n";
+}
+
+
+void transposeMatrix(vector<vector<int> > matrix){
+    vector<vector<int> > transposedMatrix (matrix.size(), vector<int>(matrix.size()));
+    for(int row = 0; row < matrix.size(); row++){
+        for(int col = row + 1; col < matrix[row].size(); col++){
+            int temp = matrix[row][col];
+            matrix[row][col] =  matrix[col][row];
+            matrix[col][row] = temp;
+        }
+    }
+
+    printMatrix(matrix);
 
 }
 
-vector<vector<int> > updateMatrix(vector<vector<int> > matrix){
 
-}
-
-
-int maxValue(vector<vector<int> > matrix){
-
-}
-
-vector<vector<int> > transposeMatrix(vector<vector<int> > matrix){
-
-}
-
-*/
 
 int main(){
     vector<int> file = readFile("matrix_input.txt");
 
     int matrixSize = file[0];
-    //cout << matrixSize;
+
+    matrix1 = vector<vector<int> >(matrixSize, vector<int>(matrixSize));
+    matrix2 = vector<vector<int> >(matrixSize, vector<int>(matrixSize));
     
     int i = 0;
-    static vector<vector<int> > matrix1 (matrixSize, vector<int>(matrixSize));
-    static vector<vector<int> > matrix2 (matrixSize, vector<int>(matrixSize));
     for(int row = 0; row < matrixSize; row++){
         for(int col = 0; col < matrixSize; col++){
             i++;
@@ -84,17 +137,15 @@ int main(){
             matrix2[row][col] = file[i + matrixSize*matrixSize];
         }
     }
+    
     printMatrix(matrix1);
     printMatrix(matrix2);
     vector<vector<int> > matrix3 = addMatrix(matrix1, matrix2);
-    printMatrix(matrix3);
+    vector<vector<int> > matrix4 = multiplyMatrix(matrix1, matrix2);
+    vector<vector<int> > matrix5 = subtractMatrix(matrix1, matrix2);
+    updateMatrix(0, 0, 18);
+    maxValue(matrix1);
+    transposeMatrix(matrix1);
 
     return 0;
 }
-
-/*
-for row
-    for col
-
-    resultingmatrix row col = matrix1row1 * matrix2 1 col
-*/
